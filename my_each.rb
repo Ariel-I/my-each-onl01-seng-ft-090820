f33e92caec10aad691d58b4cf144dae22d933be5
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 17:21:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+array = []
+
+def my_each(array)
+  i = 0 
+  while i < 4 
+  yield i 
+end 
 end
+
+my_each(array) do |number|
+  puts "#{number}"
+end 
