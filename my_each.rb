f44d2689ca8312414c5ab7284524ae71576916d0
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 18:07:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,14 +2,12 @@ array = []
 
 def my_each(array)
   i = 0 
-  collection = []
   while i < array.length 
-  collection << yield (array[i])
+ yield (array[i])
   i = i + 1 
 end 
-collection
 end
 
 my_each(array)  do |i|
-  return "#{collection}"
+  return i
 end 
