fbadb5675b643792d381ffe5deaef449dd5502a7
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 18:17:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,7 @@
 array = ["arel","jon", "Logan", "spencer"]
 
 def my_each(array)
+  if no_block_given?
   i = 0 
   collection = []
 while i < array.length 
@@ -8,6 +9,8 @@ yield (array[i])
   i = i + 1 
 end 
 collection
+else
+  "#{array}"
 end
 
 my_each(array)  
