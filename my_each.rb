39c66e05f79bafb0f5423a3341b4ff78a45e4b61
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 18:30:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,20 +2,13 @@ array = []
 
 def my_each(array)
   i = 0
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-
-def new(array)
-  i = 0
   collection = []
   while i < array.length
     collection << yield(array[i])
-    i += 1
+    i = i + 1
   end
   collection
 end
+
+
 
