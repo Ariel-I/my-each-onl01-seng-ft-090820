5372146bc31cff55183cf999c9ee354642b9bd8a
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 18:16:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,14 +3,14 @@ array = ["arel","jon", "Logan", "spencer"]
 def my_each(array)
   i = 0 
   collection = []
-  while i < array.length 
-  yield (array[i])
+while i < array.length 
+yield (array[i])
   i = i + 1 
- end 
+end 
 collection
 end
 
 my_each(array)  
-array.collect do |i|
-  return "#{i}"
+array.collect do |name|
+  return "#{name}"
 end 
